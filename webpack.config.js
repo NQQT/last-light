e11407132e76a3229477957aa08f9b6661a9390e
NQQT <nguyen.dreamax@gmail.com>
2022-07-06 00:27:19 +1000

    Update

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    alias: {
+      react: path.join(__dirname, "node_modules", "react"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx|)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".jsx"],
+        },
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
